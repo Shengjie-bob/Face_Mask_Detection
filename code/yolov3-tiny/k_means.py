536553cd3910c9f325cdf2125dad04aae9f90d15
Shengjie-bob <45875092+Shengjie-bob@users.noreply.github.com>
2021-07-30 16:17:40 +0800

    Add files via upload

---
+++ code/yolov3-tiny/k_means.py
@@ -0,0 +1,81 @@
+import numpy as np
+from tqdm import tqdm
+import torch
+
+
+def wh_iou(wh1, wh2):
+    # Returns the nxm IoU matrix. wh1 is nx2, wh2 is mx2
+    wh1 = wh1[:, None]  # [N,1,2]
+    wh2 = wh2[None]  # [1,M,2]
+    inter = torch.min(wh1, wh2).prod(2)  # [N,M]
+    return inter / (wh1.prod(2) + wh2.prod(2) - inter)  # iou = inter / (area1 + area2 - inter)
+
+
+def kmean_anchors(path='./data/coco64.txt', n=9, img_size=(640, 640), thr=0.20, gen=1000):
+    # Creates kmeans anchors for use in *.cfg files: from utils.utils import *; _ = kmean_anchors()
+    # n: number of anchors
+    # img_size: (min, max) image size used for multi-scale training (can be same values)
+    # thr: IoU threshold hyperparameter used for training (0.0 - 1.0)
+    # gen: generations to evolve anchors using genetic algorithm
+    from utils.datasets import LoadImagesAndLabels
+
+    def print_results(k):
+        k = k[np.argsort(k.prod(1))]  # sort small to large
+        iou = wh_iou(wh, torch.Tensor(k))
+        max_iou = iou.max(1)[0]
+        bpr, aat = (max_iou > thr).float().mean(), (iou > thr).float().mean() * n  # best possible recall, anch > thr
+        print('%.2f iou_thr: %.3f best possible recall, %.2f anchors > thr' % (thr, bpr, aat))
+        print('n=%g, img_size=%s, IoU_all=%.3f/%.3f-mean/best, IoU>thr=%.3f-mean: ' %
+              (n, img_size, iou.mean(), max_iou.mean(), iou[iou > thr].mean()), end='')
+        for i, x in enumerate(k):
+            print('%i,%i' % (round(x[0]), round(x[1])), end=',  ' if i < len(k) - 1 else '\n')  # use in *.cfg
+        return k
+
+    def fitness(k):  # mutation fitness
+        iou = wh_iou(wh, torch.Tensor(k))  # iou
+        max_iou = iou.max(1)[0]
+        return (max_iou * (max_iou > thr).float()).mean()  # product
+
+    # Get label wh
+    wh = []
+    dataset = LoadImagesAndLabels(path, augment=True, rect=True)
+    nr = 1 if img_size[0] == img_size[1] else 10  # number augmentation repetitions
+    for s, l in zip(dataset.shapes, dataset.labels):
+        wh.append(l[:, 3:5] * (s / s.max()))  # image normalized to letterbox normalized wh
+    wh = np.concatenate(wh, 0).repeat(nr, axis=0)  # augment 10x
+    wh *= np.random.uniform(img_size[0], img_size[1], size=(wh.shape[0], 1))  # normalized to pixels (multi-scale)
+    wh = wh[(wh > 2.0).all(1)]  # remove below threshold boxes (< 2 pixels wh)
+
+    # Kmeans calculation
+    from scipy.cluster.vq import kmeans
+    print('Running kmeans for %g anchors on %g points...' % (n, len(wh)))
+    s = wh.std(0)  # sigmas for whitening
+    k, dist = kmeans(wh / s, n, iter=30)  # points, mean distance
+    k *= s
+    wh = torch.Tensor(wh)
+    k = print_results(k)
+
+
+    # Evolve
+    npr = np.random
+    f, sh, mp, s = fitness(k), k.shape, 0.9, 0.1  # fitness, generations, mutation prob, sigma
+    for _ in tqdm(range(gen), desc='Evolving anchors'):
+        v = np.ones(sh)
+        while (v == 1).all():  # mutate until a change occurs (prevent duplicates)
+            v = ((npr.random(sh) < mp) * npr.random() * npr.randn(*sh) * s + 1).clip(0.3, 3.0)
+        kg = (k.copy() * v).clip(min=2.0)
+        fg = fitness(kg)
+        if fg > f:
+            f, k = fg, kg.copy()
+            print_results(k)
+    k = print_results(k)
+
+    return k
+
+
+#数据集聚类结果
+if __name__ == '__main__':
+
+    k = kmean_anchors(path='./data/train.txt',img_size=(320,640))
+
+    print(k)
